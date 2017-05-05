ec3016ed49e67baf982088f30de632bc15ed89e3
mms <github@ma.stoppe-gp.de>
2017-05-05 09:00:18 +0200

    Daten-Ordner ausgelagert in anderes Repo

--- data/config.inc.php
+++
@@ -1,4 +0,0 @@
-<?php
-define ("PMAP_TITLE",  "Piratenpartei Bezirk Stuttgart");
-define("PMAP_DESC", "Auf dieser Seite findest du, wo und wann im Bezirk Stuttgart überall Treffen der Piraten stattfinden.");
-?>
